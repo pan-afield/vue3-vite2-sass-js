81c85e763187a686c44418fcda23983671076412
‘pan-afield’ <15574847980@163.com>
2022-02-15 10:14:02 +0800

    first commit

---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import { useStore } from 'vuex'
+defineProps({
+  msg: String,
+});
+const count = ref(0);
+const store = useStore()
+console.log(store.state.num);
+onMounted(() => {
+  console.log('渲染完成');
+})
+</script>
+
+<template>
+  <div>Home</div>
+  <div></div>
+  <van-button type="primary">主要按钮</van-button>
+</template>
+
+<style lang="scss" scoped>
+</style>
